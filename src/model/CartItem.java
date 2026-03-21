1f8d7019db6639da2fa8868096f386645e586013
EagleForce14 <brioncyprien@orange.fr>
2026-03-22 00:40:03 +0100

    Légères améliorations #2.

--- src/model/CartItem.java
+++ src/model/CartItem.java
@@ -1,12 +1,9 @@
 package model;
 
-// Élément de panier : référence produit + quantité demandée
-// classe interne pour représenter un élément du panier
+/** Classe représentant un élément du panier d'achat du magasin.
+ * Un élément du panier est composé d'un produit et d'une quantité.
+ * **/
 public  class CartItem {
-    /** Classe représentant un élément du panier d'achat du magasin.
-     * Un élément du panier est composé d'un produit et d'une quantité.
-     * **/
-
     Product product;
     int quantity;
     // constructeur
